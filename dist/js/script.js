88947821886e729202a72105b8e203219854e5b4
LanaMaou <maulanarasyit777@gmail.com>
2023-09-10 18:05:43 +0800

    Update dark mode toggle

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,59 @@
 // Navbar Fixed
 window.onscroll = function () {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-    if (window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed');
-    } else {
-        header.classList.remove('navbar-fixed');
-    }
-}
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
+  }
+};
 
 // Hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+  setTimeout(() => {
+    navMenu.classList.toggle("muncul");
+  }, 100);
+});
+
+
+// Klik diluar hamburger
+document.addEventListener("click", function (e) {
+  if (
+    e.target !== hamburger &&
+    !hamburger.contains(e.target) &&
+    e.target !== navMenu &&
+    !navMenu.contains(e.target)
+  ) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.remove("muncul");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  darkToggle.checked ? (html.classList.add("dark"), localStorage.theme = 'dark') : (html.classList.remove("dark"), localStorage.theme = 'light');
+});
+
+
+// Pindahkan posisi toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -9,7 +10,7 @@ module.exports = {
     extend: {
       colors: {
         primary: '#14b8a6',
-        secondary: '#64748b', 
+        secondary: '#64748b',
         dark: '#0f172a'
       },
       screens: {
